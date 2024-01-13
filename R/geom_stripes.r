37fba9cca926745ccf7561d6f8dacd137bccbc31
Nicolai Karcher <karchernic@gmail.com>
2024-01-13 16:59:04 +0100

    works but is ugly

--- R/geom_stripes.r
+++ R/geom_stripes.r
@@ -59,45 +59,86 @@
 #'     colour = "black"
 #'   )
 #' print(g)
-geom_stripes <- function(mapping = NULL,
+geom_stripes_horizontal <- function(mapping = NULL,
                          data = NULL,
                          stat = "identity",
                          position = "identity",
                          ...,
                          show.legend = NA,
                          inherit.aes = TRUE) {
+    # if (is.null(along)) {
+    #   cli::cli_abort("{.fn geom_stripes} requires {.emph along} to be set.")
+    # }                                    
+    # if (!is.null(along) && !along %in% c("x", "y")) {
+    #   cli::cli_abort("{.fn geom_stripes} requires {.emph along} to be either equal to \"x\" or \"y\".")
+    # }        
   ggplot2::layer(
     data = data,
     mapping = mapping,
     stat = stat,
-    geom = GeomStripes,
+    geom = GeomStripesX,
     position = position,
     show.legend = show.legend,
     inherit.aes = inherit.aes,
-    params = list(...)
+    params = list(
+        ...
+    )
+  )
+}
+
+geom_stripes_vertical <- function(mapping = NULL,
+                         data = NULL,
+                         stat = "identity",
+                         position = "identity",
+                         ...,
+                         show.legend = NA,
+                         inherit.aes = TRUE) {
+    # if (is.null(along)) {
+    #   cli::cli_abort("{.fn geom_stripes} requires {.emph along} to be set.")
+    # }                                    
+    # if (!is.null(along) && !along %in% c("x", "y")) {
+    #   cli::cli_abort("{.fn geom_stripes} requires {.emph along} to be either equal to \"x\" or \"y\".")
+    # }        
+  ggplot2::layer(
+    data = data,
+    mapping = mapping,
+    stat = stat,
+    geom = GeomStripesY,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+        ...
+    )
   )
 }
 
 GeomStripes <- ggplot2::ggproto("GeomStripes", ggplot2::Geom,
-  required_aes = c("y"),
+    required_aes = c("y"),
+    default_aes = ggplot2::aes(
+        xmin = -Inf, xmax = Inf,
+        ymin = -Inf, ymax = Inf,
+        odd = "#22222222", even = "#00000000",
+        # Change 'size' below from 0 to NA.
+        # When not NA then when *printing in pdf device* borders are there despite
+        # requested 0th size. Seems to be some ggplot2 bug caused by grid overriding
+        # an lwd parameter somewhere, unless the size is set to NA. Found solution here
+        # https://stackoverflow.com/questions/43417514/getting-rid-of-border-in-pdf-output-for-geom-label-for-ggplot2-in-r
+        alpha = NA, colour = "black", linetype = "solid", size = NA
+    ),
+    draw_key = ggplot2::draw_key_rect
+)
 
-  default_aes = ggplot2::aes(
-    xmin = -Inf, xmax = Inf,
-    odd = "#22222222", even = "#00000000",
-    # Change 'size' below from 0 to NA.
-    # When not NA then when *printing in pdf device* borders are there despite
-    # requested 0th size. Seems to be some ggplot2 bug caused by grid overriding
-    # an lwd parameter somewhere, unless the size is set to NA. Found solution here
-    # https://stackoverflow.com/questions/43417514/getting-rid-of-border-in-pdf-output-for-geom-label-for-ggplot2-in-r
-    alpha = NA, colour = "black", linetype = "solid", size = NA
-  ),
-
-  # draw_key = ggplot2::draw_key_blank,
-  draw_key = ggplot2::draw_key_rect,
-
+GeomStripesX <- ggplot2::ggproto("GeomStripesX", GeomStripes,
   draw_panel = function(data, panel_params, coord) {
     ggplot2::GeomRect$draw_panel(
+      # in case we have facets, not every row/column of every sample will necessarily be present
+      # I resort to this hack for now, since I don't know how to access the parent data
       data %>%
+          full_join(data.frame(
+              y = attributes(panel_params$y.sec$breaks)$pos
+          ), by = "y") %>%
+          mutate(across(all_of(c("xmin", "xmax", "odd", "even", "alpha", "colour", "linetype", "size")), ~ .[!is.na(.)][1])) %>%
         dplyr::mutate(
           y = round(.data$y),
           ymin = .data$y - 0.5,
@@ -109,7 +150,7 @@ GeomStripes <- ggplot2::ggproto("GeomStripes", ggplot2::Geom,
           .data$odd, .data$even,
           .data$alpha, .data$colour, .data$linetype, .data$size
         ) %>%
-        unique() %>%
+        distinct() %>%
         dplyr::arrange(.data$ymin) %>%
         dplyr::mutate(
           .n = dplyr::row_number(),
@@ -119,9 +160,53 @@ GeomStripes <- ggplot2::ggproto("GeomStripes", ggplot2::Geom,
             false = .data$even
           )
         ) %>%
-        dplyr::select(-.data$.n, -.data$odd, -.data$even),
+        dplyr::select(-.data$.n, -.data$odd, -.data$even),       
       panel_params,
       coord
     )
   }
 )
+
+GeomStripesY <- ggplot2::ggproto("GeomStripesY", GeomStripes,
+  draw_panel = function(data, panel_params, coord) {
+    #browser()
+    ggplot2::GeomRect$draw_panel(
+      # in case we have facets, not every row/column of every sample will necessarily be present
+      # I resort to this hack for now, since I don't know how to access the parent data
+      data %>%
+          full_join(data.frame(
+              x = attributes(panel_params$x.sec$breaks)$pos
+          ), by = "x") %>%
+          mutate(across(all_of(c("ymin", "ymax", "odd", "even", "alpha", "colour", "linetype", "size")), ~ .[!is.na(.)][1])) %>%
+              arrange(x) %>%
+        dplyr::mutate(
+          x = round(.data$x),
+          xmin = .data$x - 0.5,
+          xmax = .data$x + 0.5
+        ) %>%
+        dplyr::select(
+          .data$xmin, .data$xmax,
+          .data$ymin, .data$ymax,
+          .data$odd, .data$even,
+          .data$alpha, .data$colour, .data$linetype, .data$size
+        ) %>%
+        distinct() %>%
+        dplyr::arrange(.data$ymin) %>%
+        dplyr::mutate(
+          .n = dplyr::row_number(),
+          fill = dplyr::if_else(
+            .data$.n %% 2L == 1L,
+            true = .data$odd,
+            false = .data$even
+          )
+        ) %>%
+        dplyr::select(-.data$.n, -.data$odd, -.data$even),    
+      panel_params,
+      coord
+    )
+  }
+)
+
+
+
+
--- test.r
+++ test.r
@@ -1,11 +1,31 @@
+
+# Currently only works with discrete axis
+
 library(ggplot2)
 library(tidyverse)
-library(ggstripes)
+library(devtools)
+load_all()
+#library(ggstripes)
 
 p <- ggplot(data = mtcars 
     %>% mutate(car = rownames(.))
 , aes(x = mpg, y = car)) +
     geom_point() +
-    geom_stripes(odd = "#33333333", even = "#00000000")
+    
+
+p <- ggplot(data = mtcars 
+    %>% mutate(car = rownames(.))
+, aes(x = car, y = mpg)) +
+    geom_point() +
+    geom_stripes_vertical(odd = "#33333333", even = "#00000000")
+
+p <- ggplot(data = mtcars %>% 
+    mutate(car = rownames(.)) %>%
+        mutate(group = sample(c("a", "b", "c"), size = nrow(.), replace = TRUE)) %>%
+        mutate(group = as.factor(group))
+, aes(x = mpg, y = car)) +
+    geom_point() +
+    geom_stripes_horizontal(odd = "#33333333", even = "#00000000", along = "x") +
+    facet_grid(group ~ .)
 
 p
